24b90f10d5ea98373d9fae4e25b6e04dc57bc9e7
samboateng23 <126742866+samboateng23@users.noreply.github.com>
2023-12-07 17:30:28 +0000

    IaC Files Added

---
+++ infra/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      version =">=4.9.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  profile ="sam"
+  region = "us-east-1"
+}
